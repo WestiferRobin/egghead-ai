d359be7c4edce983ced7bc28a1cff85400e8d70d
WestiferRobin <46205869+WestiferRobin@users.noreply.github.com>
2020-09-30 02:25:43 -0600

    latest attempt on Cpp file location.... damn pointers.

--- Cpp-Src/Main/main.cpp
+++ Cpp-Src/Main/main.cpp
@@ -4,6 +4,36 @@
 
 using namespace std;
 
+void buildNetwork()
+{
+    /*Node mainNode = new Node(2, 1, "mainNode");
+    Layer hiddenLayer = new Layer(this.LearningRate);
+    List<State> inputLayer = new List<State>() { new State(), new State() };
+    List<State> outputLayer = new List<State>() { new State() };
+
+    foreach(var state in inputLayer)
+        this.ConnectStateToNode(state, mainNode);
+    foreach(var state in outputLayer)
+        this.ConnectNodeToState(mainNode, state);
+
+    hiddenLayer.AddNode(mainNode);
+
+    this.Layers.Add(hiddenLayer);
+    this.OutputLayer = outputLayer;
+    this.InputLayer = inputLayer;*/
+}
+
+void trainNetwork(int iterations, vector<vector<double>> input)
+{
+    /*while (iterations > 0)
+    {
+        vector<vector<double>> inst = inputs[rand() % input.size() - 1];
+        this.RunForward(new List<double>{ inst[0], inst[1] });
+        this.RunBackward(new List<double>{ inst[2] });
+        iterations--;
+    }*/
+}
+
 int main()
 {
     return 0;
